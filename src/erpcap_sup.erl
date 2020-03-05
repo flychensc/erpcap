1d5c4cc475d155e61fc4f8bdf630ed2f1f451eb2
flychensc <flychensc@163.com>
2020-03-06 00:23:38 +0800

    睡觉

--- src/erpcap_sup.erl
+++ src/erpcap_sup.erl
@@ -29,7 +29,9 @@ init([]) ->
     SupFlags = #{strategy => one_for_all,
                  intensity => 0,
                  period => 1},
-    ChildSpecs = [],
+    AChild = {erpcap_server, {erpcap_server, start, []},
+              permanent, 2000, worker, [erpcap_server]},
+    ChildSpecs = [AChild],
     {ok, {SupFlags, ChildSpecs}}.
 
 %% internal functions
