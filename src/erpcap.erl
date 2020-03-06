5f565d1461c20e9eb6bad36ef927b92e0e2ea29d
flychensc <flychensc@163.com>
2020-03-06 22:43:12 +0800

    先调试c吧

--- src/erpcap.erl
+++ src/erpcap.erl
@@ -3,10 +3,14 @@
 
 -export([start/0]).
 
+-define(CMD_LISTIF, 1).
+-define(CMD_BINDIF, 2).
+
 start() ->
     IF_Name = application:get_env(erpcap, if_name),
     io:format("Interface Name:~w~n", [IF_Name]),
     Port = open_port({spawn, erpcap}, [{packet, 2}]),
+    Port ! {self(), {?CMD_LISTIF}},
     read_replies(Port).
 
 read_replies(Port) ->
