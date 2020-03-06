92ffb0ff7027d733a49021a3d77fac97c82c2d22
flychensc <flychensc@163.com>
2020-03-06 23:24:25 +0800

    bad value output from PORT

--- src/erpcap.erl
+++ src/erpcap.erl
@@ -10,18 +10,12 @@ start() ->
     IF_Name = application:get_env(erpcap, if_name),
     io:format("Interface Name:~w~n", [IF_Name]),
     Port = open_port({spawn, erpcap}, [{packet, 2}]),
-    Port ! {self(), {?CMD_LISTIF}},
+    Port ! {self(), {command, ?CMD_LISTIF}},
     read_replies(Port).
 
 read_replies(Port) ->
     receive
-        {Port, Any} ->
-            io:format("erlang received from port:~w~n", [Any]),
+        {Port, _Msg} ->
+            io:format("erlang received from port:~n"),
             read_replies(Port)
-    after 2000 ->
-        Port ! {self(), close},
-        receive
-            {Port, closed} ->
-                true
-        end
     end.
