df9d3620013ab0f835829d1d7367945e5f73ad3a
flychensc <flychensc@163.com>
2020-03-13 23:18:07 +0800

    gen_server大法好

--- src/erpcap.erl
+++ src/erpcap.erl
@@ -1,52 +1,62 @@
 
 -module(erpcap).
 
--export([start/1, stop/0, init/1]).
+-behaviour(gen_server).
+
+%% API
+-export([start/1, stop/0]).
 -export([send/1, reg_handler/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-record(state, {port, handlers}).
 
 start(Interface) ->
-    % io:format("start erpcap on ~w~n", [Interface]),
-    spawn(?MODULE, init, [Interface]).
+   gen_server:start_link({local, ?MODULE}, ?MODULE, Interface, []).
 
 stop() ->
-    erpcap ! stop.
+   gen_server:call(?MODULE, stop).
 
 send(Packet) ->
-    erpcap ! {send, Packet}.
+    gen_server:cast(?MODULE, {send, Packet}).
 
 reg_handler(Handler) ->
-    erpcap ! {reg, Handler}.
+    gen_server:cast(?MODULE, {reg, Handler}).
 
 init(Interface) ->
-    register(erpcap, self()),
-    process_flag(trap_exit, true),
     Command = unicode:characters_to_list(["erpcap -b ", Interface]),
     % io:format("Command:~s~n", [Command]),
     Port = open_port({spawn, Command}, [{packet, 2}]),
-    loop(Port, []).
+   {ok, #state{port=Port, handlers=[]}}.
 
-loop(Port, RxHandlers) ->
-    receive
-        % receive packet
-        {Port, {data, Data}} ->
-            handle_packet(list_to_binary(Data), RxHandlers),
-            loop(Port, RxHandlers);
-        {send, Packet} ->
-            Port ! {self(), {command, Packet}},
-            loop(Port, RxHandlers);
-        {reg, Handler} ->
-            loop(Port, lists:append(RxHandlers, [Handler]));
-        stop ->
-            Port ! {self(), close},
-            receive
-                {Port, closed} ->
-                    exit(normal)
-            end;
-        {'EXIT', Port, Reason} ->
-            io:format("exit b/z ~w~n", [Reason]),
-            exit(port_terminated)
-    end.
+handle_call(stop, _From, State) ->
+   {stop, normal, stopped, State};
 
-handle_packet(Pkt, Handlers) ->
-    % io:format("recv ~w bytes~n", [byte_size(Pkt)]),
-    lists:foreach(fun(Handler)-> Handler(Pkt) end, Handlers).
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
+
+handle_cast({reg, Handler}, State) ->
+    #state{handlers=Handlers} = State,
+   {noreply, #state{handlers=lists:append(Handlers, [Handler])}};
+
+handle_cast({send, Packet}, State) ->
+    #state{port=Port} = State,
+    Port ! {self(), {command, Packet}},
+   {noreply, State};
+
+handle_cast(_Msg, State) ->
+   {noreply, State}.
+
+handle_info({_Port, {data, Data}}, State) ->
+    #state{handlers=Handlers} = State,
+    Packet = list_to_binary(Data),
+    % io:format("recv ~w bytes~n", [byte_size(Packet)]),
+    lists:foreach(fun(Handler)-> Handler(Packet) end, Handlers),
+   {noreply, State};
+
+handle_info(_Info, State) ->
+   {noreply, State}.
+
+terminate(_Reason, _State) ->
+   ok.
+
+code_change(_OldVsn, State, _Extra) ->
+   {ok, State}.
